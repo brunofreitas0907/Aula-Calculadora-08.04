53e185bd5c555f59703630e9d019117b1581430d
brunofreitas0907 <brunosilvafreitasti0907@gmail.com>
2025-04-09 12:46:21 -0300

    Update scriptCalculadora.js

--- scriptCalculadora.js
+++ scriptCalculadora.js
@@ -1,12 +1,38 @@
+// Recebe os valores que o usuário digitou
 let valor01 = document.getElementById('caixa-valor1').value;
 let valor02 = document.getElementById('caixa-valor2').value;
 
-const botaoSoma = document.querySelector('#soma')
+// Recebe o clique do usuário no botão
+const botaoSoma = document.querySelector('#soma');
+const botaoSubtracao = document.querySelector('#subtracao');
+const botaoDivisao = document.querySelector('#divisao');
+const botaoMultiplicacao = document.querySelector('#multiplicacao');
 
+// Funções dos calculos por operação
+function subtracao()
+{
+    var valorSubtracao = valor01 - valor02;
+    return valorSubtracao
+}
 
-// Calculo da soma dos números
-botaoSoma.addEventListener('click', () => {
-    let resultadoSoma = valor01 + valor02;
-    alert(resultadoSoma)
+function divisao()
+{
+    var valorDivisao = valor01 / valor02;
+    return valorDivisao
+}
+
+function multiplicacao()
+{
+    var valorMultiplicacao = valor01 * valor02;
+    return valorMultiplicacao
+}
+
+// Retorno ao usuário escolher a opção
+botaoSoma.addEventListener("click" , () => {
+    let valor01 = document.getElementById('#caixa-valor1').value;
+    let valor02 = document.getElementById('#caixa-valor2').value;
+
+    var valorSoma = (valor01 + valor02);
+
+    console.log(valorSoma);
 });
-
