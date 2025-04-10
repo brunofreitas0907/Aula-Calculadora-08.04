267b40b6c515813ec4f611791181ef3f53620d79
brunofreitas0907 <brunosilvafreitasti0907@gmail.com>
2025-04-09 21:07:55 -0300

    Update scriptCalculadora.js

--- scriptCalculadora.js
+++ scriptCalculadora.js
@@ -1,38 +1,47 @@
-// Recebe os valores que o usuário digitou
-let valor01 = document.getElementById('caixa-valor1').value;
-let valor02 = document.getElementById('caixa-valor2').value;
-
-// Recebe o clique do usuário no botão
+// Retorno ao usuário escolher a opção Soma
 const botaoSoma = document.querySelector('#soma');
+
+botaoSoma.addEventListener("click" , () => {
+    let valor01 = document.querySelector('#valor1').value;
+    let valor02 = document.querySelector('#valor2').value;
+
+    var valorSoma = (parseFloat(valor01) + parseFloat(valor02));
+
+    document.getElementById('caixa-resultado').innerHTML = valorSoma;
+});
+
+// Retorno ao usuário escolher a opção Subtração
 const botaoSubtracao = document.querySelector('#subtracao');
+
+botaoSubtracao.addEventListener("click" , () => {
+    let valor01 = document.querySelector('#valor1').value;
+    let valor02 = document.querySelector('#valor2').value;
+
+    var valorSubtracao = (parseFloat(valor01) - parseFloat(valor02));
+
+    document.getElementById('caixa-resultado').innerHTML = valorSubtracao;
+});
+
+// Retorno ao usuário escolher a opção Divisao
 const botaoDivisao = document.querySelector('#divisao');
+
+botaoDivisao.addEventListener("click" , () => {
+    let valor01 = document.querySelector('#valor1').value;
+    let valor02 = document.querySelector('#valor2').value;
+
+    var valorDivisao = (parseFloat(valor01) / parseFloat(valor02));
+
+    document.getElementById('caixa-resultado').innerHTML = valorDivisao;
+});
+
+// Retorno ao usuário escolher a opção Multiplicacao
 const botaoMultiplicacao = document.querySelector('#multiplicacao');
 
-// Funções dos calculos por operação
-function subtracao()
-{
-    var valorSubtracao = valor01 - valor02;
-    return valorSubtracao
-}
+botaoMultiplicacao.addEventListener("click" , () => {
+    let valor01 = document.querySelector('#valor1').value;
+    let valor02 = document.querySelector('#valor2').value;
 
-function divisao()
-{
-    var valorDivisao = valor01 / valor02;
-    return valorDivisao
-}
+    var valorMultiplicacao = (parseFloat(valor01) * parseFloat(valor02));
 
-function multiplicacao()
-{
-    var valorMultiplicacao = valor01 * valor02;
-    return valorMultiplicacao
-}
-
-// Retorno ao usuário escolher a opção
-botaoSoma.addEventListener("click" , () => {
-    let valor01 = document.getElementById('#caixa-valor1').value;
-    let valor02 = document.getElementById('#caixa-valor2').value;
-
-    var valorSoma = (valor01 + valor02);
-
-    console.log(valorSoma);
+    document.getElementById('caixa-resultado').innerHTML = valorMultiplicacao;
 });
